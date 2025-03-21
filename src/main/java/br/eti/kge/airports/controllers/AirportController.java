7930fa2fa4336f0aacf0b2936b3de0a3415a09c8
“RyanSturt90s” <ryancraft2016@gmail.com>
2025-03-21 11:06:07 -0300

    Término da Fase 6 -Add classe AirportNearMeDTO -Add interface AirportNearMeProjection -Update interface AirportRepository   -add query personalizada   -add assinatura findNearMe;
    -Update classe AirportService
      -add findNearMe
    
    -Update classe AirportController
      -add endpoint /nearme com findNearMe

--- src/main/java/br/eti/kge/airports/controllers/AirportController.java
+++ src/main/java/br/eti/kge/airports/controllers/AirportController.java
@@ -5,13 +5,16 @@
 package br.eti.kge.airports.controllers;
 
 import br.eti.kge.airports.DTO.AirportMinDTO;
+import br.eti.kge.airports.DTO.AirportNearMeDTO;
 import br.eti.kge.airports.entities.Airport;
 import br.eti.kge.airports.service.AirportService;
 import java.util.List;
+import static org.apache.logging.log4j.ThreadContext.isEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,15 +30,19 @@ public class AirportController {
     
     /**
      * Endpoint /airports/city/{cityName}
-     * @param iataCode
+     * @param latitude
+     * @param longitude
      * @return
      */
-    @GetMapping("/iatacode/{iataCode}")
-    public ResponseEntity<Airport> findByIataCode(@PathVariable String iataCode) {
-        Airport result = airportService.findByIataCode(iataCode);
+    @GetMapping("/nearme")
+    public ResponseEntity<List<AirportNearMeDTO>> findByNearMe(
+            @RequestParam double latitude,
+            @RequestParam double longitude ) {
         
-        if (result == null) {
-            // Ops.. Aeroporto vazio...
+        List<AirportNearMeDTO> result = airportService.findNearMe(latitude, longitude);
+        
+        if (result.isEmpty()) {
+            // Ops.. lista vazia...
             // notFound devolve 404
             return ResponseEntity.notFound().build();
             
--- src/main/java/br/eti/kge/airports/service/AirportService.java
+++ src/main/java/br/eti/kge/airports/service/AirportService.java
@@ -5,7 +5,9 @@
 package br.eti.kge.airports.service;
 
 import br.eti.kge.airports.DTO.AirportMinDTO;
+import br.eti.kge.airports.DTO.AirportNearMeDTO;
 import br.eti.kge.airports.entities.Airport;
+import br.eti.kge.airports.projections.AirportNearMeProjection;
 import br.eti.kge.airports.repositories.AirportRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +25,17 @@ public class AirportService {
     private AirportRepository airportRepository;
     /**
      * Retorna DTO Airport por iataCode
-     * @param iataCode
+     * @param lagitude
+     * @param longitude
      * @return 
      */
     
-    public Airport findByIataCode(String iataCode) {
-        Airport result = airportRepository.findByIataCode(iataCode);
-        return result;
+    public List<AirportNearMeDTO> findNearMe(double latitude, double longitude) {
+        List<AirportNearMeProjection> resultNearAirports = airportRepository.findNearMe(latitude, longitude);
+        
+        List<AirportNearMeDTO> resultDTO = resultNearAirports.stream()
+                .map(x -> new AirportNearMeDTO(x)).toList();
+        
+        return resultDTO;
     }
 }
